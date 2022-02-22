2d8d2379c6b83e99fd455b5094802e8e67614903
Darth Noxix <47545515+DarthNoxix@users.noreply.github.com>
2022-02-21 22:17:13 -0500

    Add files via upload

--- buttoninvite/__init__.py
+++ buttoninvite/__init__.py
@@ -1,5 +1,14 @@
-from .button_invite import setup
+import json
+from pathlib import Path
+from dislash import InteractionClient
 
-__red_end_user_data_statement__ = (
-    "This cog does not persistently store data or metadata about users."
-)
+from .tickettool import TicketTool
+
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+
+
+def setup(bot):
+    bot.add_cog(TicketTool(bot))
+    if not hasattr(bot, "slash"):
+        bot.slash = InteractionClient(bot)
